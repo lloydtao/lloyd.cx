a24ffbd6e369d45d35074558e4b237f4b89374e2
LloydTao <LL23@live.co.uk>
2020-11-07 02:27:10 +0000

    Create default layout

--- portfolio/layouts/default.vue
+++ portfolio/layouts/default.vue
@@ -1,55 +1,24 @@
 <template>
   <div>
+    <!-- Backgrounds -->
+    <section class="absolute w-full h-full">
+      <div class="h-full bg-gradient-to-b from-gray-800 to-gray-900" />
+    </section>
+    <section class="absolute w-full h-full">
+      <div
+        class="h-full"
+        style="
+          background-size: 100%;
+          background-repeat: no-repeat;
+          background-image: url('bg.svg');
+        "
+      />
+    </section>
+    <!-- Components -->
+    <HeaderSlim />
     <Nuxt />
+    <FooterSlim />
   </div>
 </template>
 
-<style>
-html {
-  font-family: 'Source Sans Pro', -apple-system, BlinkMacSystemFont, 'Segoe UI',
-    Roboto, 'Helvetica Neue', Arial, sans-serif;
-  font-size: 16px;
-  word-spacing: 1px;
-  -ms-text-size-adjust: 100%;
-  -webkit-text-size-adjust: 100%;
-  -moz-osx-font-smoothing: grayscale;
-  -webkit-font-smoothing: antialiased;
-  box-sizing: border-box;
-}
-
-*,
-*::before,
-*::after {
-  box-sizing: border-box;
-  margin: 0;
-}
-
-.button--green {
-  display: inline-block;
-  border-radius: 4px;
-  border: 1px solid #3b8070;
-  color: #3b8070;
-  text-decoration: none;
-  padding: 10px 30px;
-}
-
-.button--green:hover {
-  color: #fff;
-  background-color: #3b8070;
-}
-
-.button--grey {
-  display: inline-block;
-  border-radius: 4px;
-  border: 1px solid #35495e;
-  color: #35495e;
-  text-decoration: none;
-  padding: 10px 30px;
-  margin-left: 15px;
-}
-
-.button--grey:hover {
-  color: #fff;
-  background-color: #35495e;
-}
-</style>
+<style></style>
