1dcec77b2c06ffc1b3dbc4d31c5fc85913fcdd3d
LloydTao <LL23@live.co.uk>
2020-11-07 03:31:41 +0000

    Fix hover colours in footer

--- portfolio/components/FooterSlim.vue
+++ portfolio/components/FooterSlim.vue
@@ -9,33 +9,33 @@
             Lewis Lloyd &copy; {{ date }}
           </div>
         </div>
-        <div class="w-full md:w-8/12 px-4 text-gray-200 hover:text-gray-400">
+        <div class="w-full md:w-8/12 px-4">
           <ul class="flex flex-wrap list-none md:justify-end justify-center">
             <li>
               <a
                 href="https://github.com/LloydTao/"
-                class="text-sm font-semibold block py-1 px-3"
+                class="text-gray-200 hover:text-gray-400 text-sm font-semibold block py-1 px-3"
                 >GitHub</a
               >
             </li>
             <li>
               <a
                 href="https://www.linkedin.com/in/LloydTao/"
-                class="text-sm font-semibold block py-1 px-3"
+                class="text-gray-200 hover:text-gray-400 text-sm font-semibold block py-1 px-3"
                 >LinkedIn</a
               >
             </li>
             <li>
               <a
                 href="https://www.instagram.com/lloydtao/"
-                class="text-sm font-semibold block py-1 px-3"
+                class="text-gray-200 hover:text-gray-400 text-sm font-semibold block py-1 px-3"
                 >Instagram</a
               >
             </li>
             <li>
               <a
                 href="https://twitter.com/LloydTao/"
-                class="text-sm font-semibold block py-1 px-3"
+                class="text-gray-200 hover:text-gray-400 text-sm font-semibold block py-1 px-3"
                 >Twitter</a
               >
             </li>
