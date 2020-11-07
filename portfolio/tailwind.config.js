0b21c3ef0fda96f5c0e2f97449055d4e253c4a3f
LloydTao <LL23@live.co.uk>
2020-11-07 02:24:41 +0000

    Configure Tailwind theme

--- portfolio/tailwind.config.js
+++ portfolio/tailwind.config.js
@@ -5,7 +5,18 @@
  ** Default: https://github.com/tailwindcss/tailwindcss/blob/master/stubs/defaultConfig.stub.js
  */
 module.exports = {
-  theme: {},
+  theme: {
+    screens: {
+      sm: '640px',
+      md: '768px',
+      lg: '1024px',
+      xl: '1280px',
+    },
+    fontFamily: {
+      body: ['Raleway', 'sans-serif'],
+      hero: ['Raleway', 'sans-serif'],
+    },
+  },
   variants: {},
   plugins: [],
 }
