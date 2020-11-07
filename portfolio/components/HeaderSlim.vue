7d5d14627f1b6c32ba835aa683e6cad0f41f3c2f
LloydTao <LL23@live.co.uk>
2020-11-07 03:32:37 +0000

    Create header component

---
+++ portfolio/components/HeaderSlim.vue
@@ -0,0 +1,23 @@
+<template>
+  <nav
+    class="top-0 absolute z-50 w-full flex flex-wrap items-center justify-between px-2 py-3 navbar-expand-lg"
+  >
+    <div class="container mx-auto px-4">
+      <div
+        class="flex flex-wrap items-center md:justify-between justify-center"
+      >
+        <div class="w-full md:w-4/12 px-4">
+          <div
+            class="text-gray-200 hover:text-gray-400 text-sm font-semibold py-1"
+          >
+            <NuxtLink to="/">lloyd.cx</NuxtLink>
+          </div>
+        </div>
+      </div>
+    </div>
+  </nav>
+</template>
+
+<script>
+export default {}
+</script>
