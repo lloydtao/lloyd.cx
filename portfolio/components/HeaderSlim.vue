bed7958869115847fdfb258a3caf665e435ad8fd
LloydTao <LL23@live.co.uk>
2020-11-07 03:49:21 +0000

    Make header font size regular

--- portfolio/components/HeaderSlim.vue
+++ portfolio/components/HeaderSlim.vue
@@ -7,9 +7,7 @@
         class="flex flex-wrap items-center md:justify-between justify-center"
       >
         <div class="w-full md:w-4/12 px-4">
-          <div
-            class="text-gray-200 hover:text-gray-400 text-sm font-semibold py-1"
-          >
+          <div class="text-gray-200 hover:text-gray-400 font-semibold py-1">
             <NuxtLink to="/">lloyd.cx</NuxtLink>
           </div>
         </div>
