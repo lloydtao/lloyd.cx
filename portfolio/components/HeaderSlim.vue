9fd839334464a2db0c4eb5cb17dcfb39d9b9ef3e
LloydTao <LL23@live.co.uk>
2020-11-22 12:54:10 +0000

    Create base responsive navbar

--- portfolio/components/HeaderSlim.vue
+++ portfolio/components/HeaderSlim.vue
@@ -7,9 +7,40 @@
         class="flex flex-wrap items-center md:justify-between justify-center"
       >
         <div class="w-full md:w-4/12 px-4">
-          <div class="text-gray-200 hover:text-gray-400 font-semibold py-1">
-            <NuxtLink to="/">lloyd.cx</NuxtLink>
-          </div>
+          <ul class="flex flex-wrap list-none md:justify-start justify-center">
+            <li>
+              <NuxtLink
+                to="/"
+                class="text-gray-200 hover:text-gray-400 text-xl font-semibold py-1"
+                >lloyd.cx</NuxtLink
+              >
+            </li>
+          </ul>
+        </div>
+        <div class="w-full md:w-8/12 px-4">
+          <ul class="flex flex-wrap list-none md:justify-end justify-center">
+            <li>
+              <NuxtLink
+                to="/"
+                class="text-gray-200 hover:text-gray-400 text-sm font-semibold py-1 px-3"
+                >Portfolio</NuxtLink
+              >
+            </li>
+            <li>
+              <NuxtLink
+                to="/"
+                class="text-gray-200 hover:text-gray-400 text-sm font-semibold py-1 px-3"
+                >Open Source</NuxtLink
+              >
+            </li>
+            <li>
+              <NuxtLink
+                to="/"
+                class="text-gray-200 hover:text-gray-400 text-sm font-semibold py-1 px-3"
+                >Lectures</NuxtLink
+              >
+            </li>
+          </ul>
         </div>
       </div>
     </div>
