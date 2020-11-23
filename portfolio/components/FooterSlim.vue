b31ebe4efd8cc5b3d1a3c5e6fbf6e27e8bc82219
LloydTao <LL23@live.co.uk>
2020-11-23 10:16:42 +0000

    Remove horizontal padding in footer

--- portfolio/components/FooterSlim.vue
+++ portfolio/components/FooterSlim.vue
@@ -4,7 +4,7 @@
       <div
         class="flex flex-wrap items-center md:justify-between justify-center"
       >
-        <div class="w-full md:w-4/12 px-4 mt-2">
+        <div class="w-full md:w-4/12 mt-2">
           <ul class="flex flex-wrap list-none md:justify-start justify-center">
             <li>
               <a class="text-gray-300 text-sm font-semibold py-1"
@@ -13,7 +13,7 @@
             </li>
           </ul>
         </div>
-        <div class="w-full md:w-8/12 px-4 mt-2">
+        <div class="w-full md:w-8/12 mt-2">
           <ul class="flex flex-wrap list-none md:justify-end justify-center">
             <li>
               <a
