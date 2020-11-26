3c8981d52810b3f93a1c08c3c30e6b53ca38482c
LloydTao <LL23@live.co.uk>
2020-11-26 20:55:07 +0000

    Make quote text darker

--- portfolio/components/ProfileHero.vue
+++ portfolio/components/ProfileHero.vue
@@ -67,7 +67,7 @@
             alt="L-sit static hold in Harrison Study Room"
           />
           <div
-            class="text-xl p-8 bg-gray-100 text-gray-600 italic rounded-b-lg"
+            class="text-xl p-8 bg-gray-100 text-gray-700 italic rounded-b-lg"
           >
             &quot;there’s two types of people, either you can punch in your
             dreams or you can’t&quot;
