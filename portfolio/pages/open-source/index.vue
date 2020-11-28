52a01b3c560fe18e019ec3c703abfc04b04fc47e
LloydTao <LL23@live.co.uk>
2020-11-28 00:47:15 +0000

    Create pages for all navbar links

---
+++ portfolio/pages/open-source/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="container mx-auto pt-4 pb-8 px-8 md:px-16">
+    <h1 class="text-4xl md:text-5xl m-1 text-gray-100 uppercase font-black">
+      Open Source
+    </h1>
+    <p class="text-xl m-1 text-gray-200">Content coming soon. I promise.</p>
+    <CardRow />
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style></style>
