42c2abc58480f48a28efeb6bad03bca26bb15942
Lewis Lloyd <41923457+LloydTao@users.noreply.github.com>
2021-04-10 14:17:04 +0100

    Update tests for new component names

--- portfolio/test/HeaderSlim.spec.js
+++
@@ -1,9 +0,0 @@
-import { mount } from '@vue/test-utils'
-import HeaderSlim from '@/components/HeaderSlim.vue'
-
-describe('HeaderSlim', () => {
-  test('is a Vue instance', () => {
-    const wrapper = mount(HeaderSlim)
-    expect(wrapper.vm).toBeTruthy()
-  })
-})
