7abacc5b0dbcaaadd9344a756da3f0d0de1abffc
Lewis Lloyd <41923457+LloydTao@users.noreply.github.com>
2021-04-15 17:40:51 +0100

    Make footer mobile-friendly

--- portfolio/components/Footer.vue
+++ portfolio/components/Footer.vue
@@ -1,52 +1,42 @@
 <template>
-  <footer class="w-full bottom-0 pb-6 mt-2">
-    <div class="container mx-auto px-4">
-      <div
-        class="flex flex-wrap items-center md:justify-between justify-center"
-      >
-        <div class="w-full md:w-4/12 mt-2">
-          <ul class="flex flex-wrap list-none md:justify-start justify-center">
-            <li>
-              <div class="text-gray-300 text-sm font-semibold py-1">
-                Lewis Lloyd &copy; {{ date }}
-              </div>
-            </li>
-          </ul>
+  <footer class="flex flex-col sm:flex-row container mx-auto px-5 py-5">
+    <ul class="flex flex-grow mx-auto mt-5">
+      <li>
+        <div class="text-gray-300 text-sm font-semibold py-1">
+          Lewis Lloyd &copy; {{ date }}
         </div>
-        <div class="w-full md:w-8/12 mt-2">
-          <ul class="flex flex-wrap list-none md:justify-end justify-center">
-            <li>
-              <a
-                href="https://github.com/LloydTao/"
-                class="text-gray-300 hover:text-gray-500 text-sm block py-1 px-3"
-                >GitHub</a
-              >
-            </li>
-            <li>
-              <a
-                href="https://www.linkedin.com/in/LloydTao/"
-                class="text-gray-300 hover:text-gray-500 text-sm block py-1 px-3"
-                >LinkedIn</a
-              >
-            </li>
-            <li>
-              <a
-                href="https://www.instagram.com/lloydtao/"
-                class="text-gray-300 hover:text-gray-500 text-sm block py-1 px-3"
-                >Instagram</a
-              >
-            </li>
-            <li>
-              <a
-                href="https://twitter.com/LloydTao/"
-                class="text-gray-300 hover:text-gray-500 text-sm block py-1 px-3"
-                >Twitter</a
-              >
-            </li>
-          </ul>
-        </div>
-      </div>
-    </div>
+      </li>
+    </ul>
+    <ul class="flex flex-none mx-auto mt-3">
+      <li>
+        <a
+          href="https://github.com/LloydTao/"
+          class="text-gray-300 hover:text-gray-400 text-sm block py-1 px-3"
+          >GitHub</a
+        >
+      </li>
+      <li>
+        <a
+          href="https://www.linkedin.com/in/LloydTao/"
+          class="text-gray-300 hover:text-gray-400 text-sm block py-1 px-3"
+          >LinkedIn</a
+        >
+      </li>
+      <li>
+        <a
+          href="https://www.instagram.com/lloydtao/"
+          class="text-gray-300 hover:text-gray-400 text-sm block py-1 px-3"
+          >Instagram</a
+        >
+      </li>
+      <li>
+        <a
+          href="https://twitter.com/LloydTao/"
+          class="text-gray-300 hover:text-gray-500 text-sm block py-1 px-3"
+          >Twitter</a
+        >
+      </li>
+    </ul>
   </footer>
 </template>
 
