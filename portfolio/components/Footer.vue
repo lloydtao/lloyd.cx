089be3f14fe8cd87eb899bf2166849f69e3d5f05
Lewis Lloyd <41923457+LloydTao@users.noreply.github.com>
2021-04-15 17:44:57 +0100

    Add margin to footer

--- portfolio/components/Footer.vue
+++ portfolio/components/Footer.vue
@@ -1,5 +1,5 @@
 <template>
-  <footer class="flex flex-col sm:flex-row container mx-auto px-5 py-5">
+  <footer class="flex flex-col sm:flex-row container mx-auto px-5 py-5 mt-3">
     <ul class="flex flex-grow mx-auto mt-5">
       <li>
         <div class="text-gray-300 text-sm font-semibold py-1">
