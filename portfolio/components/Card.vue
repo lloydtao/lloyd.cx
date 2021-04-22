b09a3c7abd3b3046817657e64e7ef4b488b7ba5f
Lewis Lloyd <41923457+LloydTao@users.noreply.github.com>
2021-04-23 00:23:01 +0100

    Create post card component

---
+++ portfolio/components/Card.vue
@@ -0,0 +1,17 @@
+<template>
+  <li class="w-full md:w-1/2 xl:w-1/3 mb-2 px-1">
+    <div class="bg-gray-900 bg-opacity-50 px-5 py-4">
+      <p class="font-semibold text-white">{{ title }}</p>
+      <p class="text-gray-100 mt-3">{{ description }}</p>
+    </div>
+  </li>
+</template>
+
+<script>
+export default {
+  props: {
+    title: { type: String, default: 'Title' },
+    description: { type: String, default: 'Description' },
+  },
+}
+</script>
