f274c6105db4e982f9cb4ec176bcf5c7d39a4199
Lewis Lloyd <41923457+LloydTao@users.noreply.github.com>
2021-04-23 01:53:12 +0100

    Refactor blog post into component

--- portfolio/pages/articles/_slug.vue
+++ portfolio/pages/articles/_slug.vue
@@ -1,17 +1,5 @@
 <template>
-  <div class="container mx-auto px-5">
-    <h1 class="text-4xl text-gray-100 font-black mt-5">
-      {{ post.title }}
-    </h1>
-    <p class="text-xl text-gray-200 mt-3">
-      {{ post.description }}
-    </p>
-    <div
-      class="max-w-4xl bg-gray-100 rounded px-4 py-5 md:px-8 md:py-8 mt-8 mx-auto"
-    >
-      <NuxtContent class="prose prose-sm md:prose-lg" :document="post" />
-    </div>
-  </div>
+  <Post :post="post" />
 </template>
 
 <script>
