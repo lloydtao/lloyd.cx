839b1342aa2d2baff0806b693f30b073418ce9ac
Lewis Lloyd <41923457+LloydTao@users.noreply.github.com>
2021-04-23 01:42:23 +0100

    Add test post for portfolio

--- portfolio/pages/portfolio/index.vue
+++ portfolio/pages/portfolio/index.vue
@@ -3,13 +3,23 @@
     <h1 class="text-4xl text-gray-100 uppercase font-black mt-5">Portfolio</h1>
     <p class="text-xl text-gray-200 mt-3">What I've been doing since 1999.</p>
     <div class="mt-5">
-      <CardGrid :posts="posts" route="articles-slug" />
+      <CardGrid :posts="posts" route="portfolio-slug" />
     </div>
   </div>
 </template>
 
 <script>
-export default {}
+export default {
+  async asyncData({ $content, params }) {
+    const posts = await $content('portfolio')
+      .only(['slug', 'title', 'description'])
+      .sortBy('createdAt', 'asc')
+      .fetch()
+    return {
+      posts,
+    }
+  },
+}
 </script>
 
 <style></style>
