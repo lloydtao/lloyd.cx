1282a1a3ab4003e80322567c29e6e31aba87eb70
Lewis Lloyd <41923457+LloydTao@users.noreply.github.com>
2021-04-23 01:32:18 +0100

    Match portfolio page with articles

--- portfolio/pages/portfolio/index.vue
+++ portfolio/pages/portfolio/index.vue
@@ -1,8 +1,10 @@
 <template>
   <div class="container mx-auto px-5">
-    <h1 class="text-5xl text-gray-100 uppercase font-black mt-5">Portfolio</h1>
-    <p class="text-xl text-gray-200">Content coming soon. I promise.</p>
-    <CardRow />
+    <h1 class="text-4xl text-gray-100 uppercase font-black mt-5">Portfolio</h1>
+    <p class="text-xl text-gray-200 mt-3">What I've been doing since 1999.</p>
+    <div class="mt-5">
+      <CardGrid :posts="posts" route="articles-slug" />
+    </div>
   </div>
 </template>
 
