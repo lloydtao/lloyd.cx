a60ef9897c3c073888e621e0a7052fde7978d9dd
LloydTao <41923457+LloydTao@users.noreply.github.com>
2021-11-21 17:39:36 +0000

    Deploying to build from  @ 30113f0f05c66c124d6252f2d67cc06ac6792e39 🚀

--- _nuxt/static/1620056973/portfolio/payload.js
+++
@@ -1,1 +0,0 @@
-__NUXT_JSONP__("/portfolio", {data:[{posts:[{slug:"test-project",description:"This is the description for a test project.",title:"Test project"}]}],fetch:{},mutations:void 0});
---
+++ _nuxt/static/1637516373/articles/markdown-blog-with-nuxt-03-detail-view/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1637516373",layout:"default",error:a,serverRendered:true,routePath:"\u002Farticles\u002Fmarkdown-blog-with-nuxt-03-detail-view",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"3b77ae8f"}}}}(null));
