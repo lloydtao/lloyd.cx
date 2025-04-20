94937be53983e0bbcc8385abdf3111e574c8d543
Lewis Lloyd <lewis_lloyd@live.co.uk>
2025-04-20 18:31:05 +0100

    Install nuxt-twemoji

--- portfolio/nuxt.config.ts
+++ portfolio/nuxt.config.ts
@@ -2,6 +2,11 @@
 export default defineNuxtConfig({
   compatibilityDate: "2024-11-01",
   devtools: { enabled: true },
-  modules: ["@nuxt/eslint", "@nuxt/image", "@nuxtjs/tailwindcss"],
+  modules: [
+    "@nuxt/eslint",
+    "@nuxt/image",
+    "@nuxtjs/tailwindcss",
+    "nuxt-twemoji",
+  ],
   image: {},
 });
