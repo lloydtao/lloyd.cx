9c01e80b3fe017be012a5f4050c35bc0c4c8bee6
Lewis Lloyd <lewis_lloyd@live.co.uk>
2025-07-08 23:42:43 +0100

    Configure Nuxt Content

--- portfolio/nuxt.config.ts
+++ portfolio/nuxt.config.ts
@@ -1,8 +1,12 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: "2024-11-01",
+  content: {
+    experimental: { nativeSqlite: true },
+  },
   devtools: { enabled: true },
   modules: [
+    '@nuxt/content',
     "@nuxt/eslint",
     "@nuxt/image",
     "@nuxtjs/tailwindcss",
