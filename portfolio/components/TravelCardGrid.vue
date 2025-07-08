f6945635d65e130725a73203bbf2f072313b7351
Lewis Lloyd <lewis_lloyd@live.co.uk>
2025-07-08 23:32:57 +0100

    Compress travel cards into one grid

--- portfolio/components/TravelCardGrid.vue
+++ portfolio/components/TravelCardGrid.vue
@@ -1,7 +1,5 @@
 <template>
   <section>
-    <h1 class="mt-8 text-xl font-semibold text-slate-100">{{ year }}</h1>
-
     <div
       class="mt-3 grid grid-cols-1 gap-5 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5"
     >
@@ -14,7 +12,6 @@
 import type { TravelCardProps } from "~/types/travel-card-props";
 
 interface Props {
-  year: number;
   cards: TravelCardProps[];
 }
 defineProps<Props>();
