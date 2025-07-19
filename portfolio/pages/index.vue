d88cd0a00a1a150744c2ebb8f021bca09de7da68
Lewis Lloyd <lewis_lloyd@live.co.uk>
2025-07-19 23:15:57 +0100

    Add recent trips to home page

--- portfolio/pages/index.vue
+++ portfolio/pages/index.vue
@@ -3,10 +3,26 @@
     <div>
       <HomeHero />
     </div>
+    <div class="container mx-auto mt-3 px-3 py-5">
+      <div class="mb-4 flex items-center justify-between">
+        <h1 class="text-xl font-semibold text-slate-50 md:text-2xl">
+          Recent trips
+        </h1>
+      </div>
+      <div id="travel-grid-card">
+        <TravelCardGrid :cards="travelCardGrid" />
+      </div>
+    </div>
   </div>
 </template>
 
 <script setup lang="ts">
+const route = useRoute();
+
+const { data: travelCardGrid } = await useAsyncData(route.path, () => {
+  return queryCollection("trips").order("startDate", "DESC").limit(6).all();
+});
+
 useSeoMeta({
   title: "Home • lloyd.cx",
   ogTitle: "Home",
