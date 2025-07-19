ddbd170a9c07277b2f92467e300004dc21917142
Lewis Lloyd <41923457+lloydtao@users.noreply.github.com>
2025-07-20 00:28:39 +0100

    feat: recent trips, Norway photos and Portugal photos (#46)
    * Adjust time on Lithuania photo
    
    * Add new Lithuania photo
    
    * Remove one Poland photo
    
    * Populate Norway photos
    
    * Populate Portugal photos
    
    * Add recent trips to home page
    
    * Add "view all" trips button
    
    * Tweak colour of heading

--- portfolio/pages/index.vue
+++ portfolio/pages/index.vue
@@ -3,10 +3,32 @@
     <div>
       <HomeHero />
     </div>
+    <div class="container mx-auto mt-3 px-3 py-5">
+      <div class="mb-4 flex items-center justify-between">
+        <h1 class="text-xl font-semibold text-slate-100 md:text-2xl">
+          Recent trips
+        </h1>
+        <NuxtLink
+          to="/travel/"
+          class="relative rounded-2xl border border-white/20 bg-white/10 px-3 py-1 text-sm font-medium text-slate-200 shadow-lg backdrop-blur-lg transition duration-200 hover:bg-white/20 focus:outline-none focus:ring-2 focus:ring-indigo-400 md:px-5 md:text-base"
+        >
+          View all
+        </NuxtLink>
+      </div>
+      <div id="travel-grid-card">
+        <TravelCardGrid :cards="travelCardGrid" />
+      </div>
+    </div>
   </div>
 </template>
 
 <script setup lang="ts">
+const route = useRoute();
+
+const { data: travelCardGrid } = await useAsyncData(route.path, () => {
+  return queryCollection("trips").order("startDate", "DESC").limit(6).all();
+});
+
 useSeoMeta({
   title: "Home • lloyd.cx",
   ogTitle: "Home",
