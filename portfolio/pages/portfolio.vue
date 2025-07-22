d86996094d0826c930eb2e0c0df8e38574cf2ecf
Lewis Lloyd <lewis_lloyd@live.co.uk>
2025-07-22 23:26:50 +0100

    Flesh out placeholder portfolio page

--- portfolio/pages/portfolio.vue
+++ portfolio/pages/portfolio.vue
@@ -7,34 +7,196 @@
       <div class="mt-5">
         <h1 class="text-xl font-semibold text-slate-100">Experience</h1>
       </div>
-      <div class="mt-3">
-        <div
-          class="flex items-center overflow-hidden rounded-2xl border border-white/20 bg-white/10 backdrop-blur-lg transition hover:bg-white/20"
-        >
-          <div>
-            <NuxtImg
-              class="w-32"
-              height="512"
-              width="512"
-              src="/portfolio/deliveroo/logo.jpg"
-            />
+      <div class="mb-8 mt-5 grid gap-3 lg:grid-cols-2 lg:gap-5">
+        <div>
+          <div
+            class="flex items-center overflow-hidden rounded-2xl border border-white/20 bg-white/10 backdrop-blur-lg transition hover:bg-white/20"
+          >
+            <div>
+              <NuxtImg
+                class="w-32"
+                height="512"
+                width="512"
+                src="/portfolio/experience/deliveroo/logo.jpg"
+              />
+            </div>
+            <div class="w-full max-w-lg space-y-4">
+              <ul class="flex flex-col space-y-4">
+                <li>
+                  <div class="px-5 py-2">
+                    <h3 class="font-semibold text-slate-100 md:text-lg">
+                      Deliveroo
+                    </h3>
+                    <p class="text-sm text-slate-200 md:text-base">
+                      Software Engineer II
+                    </p>
+                    <p class="mt-2 text-xs text-slate-300 md:text-sm">
+                      Jul 2024 – Present
+                    </p>
+                  </div>
+                </li>
+              </ul>
+            </div>
           </div>
-          <div class="w-full max-w-lg space-y-4">
-            <ul class="flex flex-col space-y-4">
-              <li>
-                <div class="px-5 py-2">
-                  <h3 class="font-semibold text-slate-100 md:text-lg">
-                    Deliveroo
-                  </h3>
-                  <p class="text-sm text-slate-200 md:text-base">
-                    Software Engineer II
-                  </p>
-                  <p class="mt-2 text-xs text-slate-300 md:text-sm">
-                    July 2024 – Present
-                  </p>
-                </div>
-              </li>
-            </ul>
+        </div>
+        <div>
+          <div
+            class="flex items-center overflow-hidden rounded-2xl border border-white/20 bg-white/10 backdrop-blur-lg transition hover:bg-white/20"
+          >
+            <div>
+              <NuxtImg
+                class="w-32"
+                height="512"
+                width="512"
+                src="/portfolio/experience/recruit-hub/logo.jpg"
+              />
+            </div>
+            <div class="w-full max-w-lg space-y-4">
+              <ul class="flex flex-col space-y-4">
+                <li>
+                  <div class="px-5 py-2">
+                    <h3 class="font-semibold text-slate-100 md:text-lg">
+                      RecruitHub
+                    </h3>
+                    <p class="text-sm text-slate-200 md:text-base">
+                      Software Engineer
+                    </p>
+                    <p class="mt-2 text-xs text-slate-300 md:text-sm">
+                      Feb 2023 – Jul 2024
+                    </p>
+                  </div>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+        <div>
+          <div
+            class="flex items-center overflow-hidden rounded-2xl border border-white/20 bg-white/10 backdrop-blur-lg transition hover:bg-white/20"
+          >
+            <div>
+              <NuxtImg
+                class="w-32"
+                height="512"
+                width="512"
+                src="/portfolio/experience/magpie-education/logo.webp"
+              />
+            </div>
+            <div class="w-full max-w-lg space-y-4">
+              <ul class="flex flex-col space-y-4">
+                <li>
+                  <div class="px-5 py-2">
+                    <h3 class="font-semibold text-slate-100 md:text-lg">
+                      Magpie Education
+                    </h3>
+                    <p class="text-sm text-slate-200 md:text-base">
+                      Full Stack Developer
+                    </p>
+                    <p class="mt-2 text-xs text-slate-300 md:text-sm">
+                      Jun 2020 – Feb 2023
+                    </p>
+                  </div>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+        <div>
+          <div
+            class="flex items-center overflow-hidden rounded-2xl border border-white/20 bg-white/10 backdrop-blur-lg transition hover:bg-white/20"
+          >
+            <div>
+              <NuxtImg
+                class="w-32"
+                height="512"
+                width="512"
+                src="/portfolio/experience/hatless-studios/logo.jpg"
+              />
+            </div>
+            <div class="w-full max-w-lg space-y-4">
+              <ul class="flex flex-col space-y-4">
+                <li>
+                  <div class="px-5 py-2">
+                    <h3 class="font-semibold text-slate-100 md:text-lg">
+                      Hatless Studios
+                    </h3>
+                    <p class="text-sm text-slate-200 md:text-base">
+                      Software Engineer
+                    </p>
+                    <p class="mt-2 text-xs text-slate-300 md:text-sm">
+                      Feb 2020 – Oct 2020
+                    </p>
+                  </div>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="mt-5">
+        <h1 class="text-xl font-semibold text-slate-100">Projects</h1>
+      </div>
+      <div class="mb-8 mt-5 grid gap-3 lg:grid-cols-2 lg:gap-5">
+        <div>
+          <div
+            class="flex items-center overflow-hidden rounded-2xl border border-white/20 bg-white/10 backdrop-blur-lg transition hover:bg-white/20"
+          >
+            <div>
+              <NuxtImg
+                class="w-32"
+                height="512"
+                width="512"
+                src="/portfolio/projects/duflist/logo.jpg"
+              />
+            </div>
+            <div class="w-full max-w-lg space-y-4">
+              <ul class="flex flex-col space-y-4">
+                <li>
+                  <div class="px-5 py-2">
+                    <h3 class="font-semibold text-slate-100 md:text-lg">
+                      Duflist
+                    </h3>
+                    <p class="text-sm text-slate-200 md:text-base">
+                      Social media application
+                    </p>
+                    <p class="mt-2 text-xs text-slate-300 md:text-sm">
+                      Jul 2025 – Present
+                    </p>
+                  </div>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+        <div>
+          <div
+            class="flex items-center overflow-hidden rounded-2xl border border-white/20 bg-white/10 backdrop-blur-lg transition hover:bg-white/20"
+          >
+            <div>
+              <NuxtImg
+                class="w-32"
+                height="512"
+                width="512"
+                src="/portfolio/projects/gridlink/logo.jpg"
+              />
+            </div>
+            <div class="w-full max-w-lg space-y-4">
+              <ul class="flex flex-col space-y-4">
+                <li>
+                  <div class="px-5 py-2">
+                    <h3 class="font-semibold text-slate-100 md:text-lg">
+                      Gridlink
+                    </h3>
+                    <p class="text-sm text-slate-200 md:text-base">
+                      Social media application
+                    </p>
+                    <p class="mt-2 text-xs text-slate-300 md:text-sm">
+                      Nov 2022 – Present
+                    </p>
+                  </div>
+                </li>
+              </ul>
+            </div>
           </div>
         </div>
       </div>
