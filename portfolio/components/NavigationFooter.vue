09a96fe3f8e24858eea6e7de8d1c451d445f49a1
Lewis Lloyd <lewis_lloyd@live.co.uk>
2025-08-26 21:37:10 +0100

    Fix footer links padding

--- portfolio/components/NavigationFooter.vue
+++ portfolio/components/NavigationFooter.vue
@@ -9,11 +9,11 @@
         </div>
       </li>
     </ul>
-    <ul class="mx-auto mt-3 flex flex-none">
+    <ul class="mx-auto mt-3 flex flex-none space-x-5">
       <li>
         <NuxtLink
           href="https://www.linkedin.com/in/LloydTao/"
-          class="px-3 py-1 text-sm font-semibold text-gray-50 duration-100 hover:text-gray-400"
+          class="text-sm font-semibold text-gray-50 duration-100 hover:text-gray-400"
           external
         >
           LinkedIn
@@ -22,7 +22,7 @@
       <li>
         <NuxtLink
           href="https://www.instagram.com/lloydtao/"
-          class="px-3 py-1 text-sm font-semibold text-gray-50 duration-100 hover:text-gray-400"
+          class="text-sm font-semibold text-gray-50 duration-100 hover:text-gray-400"
           external
         >
           Instagram
