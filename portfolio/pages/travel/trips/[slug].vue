81aeb8ac4d914e540df62444d14da88f0e233a32
Lewis Lloyd <41923457+lloydtao@users.noreply.github.com>
2026-02-24 00:55:06 +0000

    feat: enhance image resolution, and fix lightbox (#85)
    * Fix resolution of 300x400 images
    
    * Use standard img for lightbox

--- portfolio/pages/travel/trips/[slug].vue
+++ portfolio/pages/travel/trips/[slug].vue
@@ -107,8 +107,8 @@
                       :src="photo.photoUrl"
                       :alt="photo.title"
                       class="h-full w-full object-cover"
-                      height="400"
-                      width="300"
+                      height="800"
+                      width="600"
                     />
                   </button>
                 </div>
@@ -137,7 +137,7 @@
         @click.self="closeLightbox"
       >
         <div class="relative max-h-[90vh] max-w-[90vw]">
-          <NuxtImg
+          <img
             :src="lightboxPhoto.photoUrl"
             :alt="lightboxPhoto.title"
             class="h-auto max-h-[90vh] w-auto max-w-[90vw] rounded-2xl object-contain"
