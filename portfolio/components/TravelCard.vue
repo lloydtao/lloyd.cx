b9eaaaa95d167347e3b4ada6a18f9675388882c7
Lewis Lloyd <41923457+lloydtao@users.noreply.github.com>
2026-05-09 17:29:49 +0100

    Fix rotating images travel cards

--- portfolio/components/TravelCard.vue
+++ portfolio/components/TravelCard.vue
@@ -9,6 +9,7 @@
         class="absolute inset-0 h-full w-full object-cover"
         height="800"
         width="600"
+        :modifiers="{ rotate: null }"
       />
       <div
         class="pointer-events-none absolute inset-0 z-0 rounded-2xl bg-slate-900/10 opacity-0 transition duration-100 group-hover:opacity-100 group-hover:shadow-[0_0_25px_4px_rgba(255,255,255,0.6)]"
